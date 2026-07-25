06dd9de790b2f2d01282f2d9d9fddb150a5532ed
Prince Choudhary <153719179+prince109iiita@users.noreply.github.com>
2026-07-25 12:39:27 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@ require("dotenv").config()
 const app = require("../Backend/src/app")
 const connectToDB = require("../Backend/src/config/database")
 
-connectToDB()
+connectToDB().catch((err) => console.error("Initial DB connection attempt failed:", err.message))
 
 module.exports = app
